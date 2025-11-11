8a87bad4ec66c62a3440c4acfb8d7dd89780d417
Lorenz Hänggi <lolo8304@gmail.com>
2025-11-11 16:16:43 +0100

    refactor + split code

---
+++ src/tool.anonymize.text.ts
@@ -0,0 +1,36 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { anonymizeObject } from "./anonymizer";
+import { z } from "zod";
+
+export function anonymizeTextTool(server: McpServer) {
+  server.registerTool(
+    "anonymize-text",
+    {
+      title: "Anonymize Data",
+      description: "Anonymize some objects by removing personal information.",
+      inputSchema: { data: z.any() },
+      outputSchema: { anonymizedData: z.any() },
+      annotations: {
+        destructiveHint: false,
+        requiresUserConsent: false,
+        readOnlyHint: true,
+        idempotentHint: false,
+        openWorldHint: false,
+      },
+    },
+    async ({ data }) => {
+      const transformed = await anonymizeObject(data);
+      console.log(transformed);
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify({ anonymizedData: transformed }),
+            mimeType: "application/json",
+          },
+        ],
+        structuredContent: { anonymizedData: transformed },
+      };
+    }
+  );
+}
